81b862315e0969858ff84e9060b1247e60a5ab2a
Adam Salih <adam.salih@thefuntasty.com>
2020-05-12 22:12:30 +0200

    Add SimpleJSON library

--- src/common/Loader.java
+++ src/common/Loader.java
@@ -1,30 +1,34 @@
-//package common;
-//
-//import java.io.FileReader;
-//import org.json.simple.parser.JSONParser;
-//
-//public class Loader {
-//    public Loader() {
-//        JSONParser parser = new JSONParser();
-//        try {
-//            Object obj = parser.parse(new FileReader("/Users/Shared/crunchify.json"));
-//
-//            // A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
-//            JSONObject jsonObject = (JSONObject) obj;
-//
-//            // A JSON array. JSONObject supports java.util.List interface.
-//            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
-//
-//            // An iterator over a collection. Iterator takes the place of Enumeration in the Java Collections Framework.
-//            // Iterators differ from enumerations in two ways:
-//            // 1. Iterators allow the caller to remove elements from the underlying collection during the iteration with well-defined semantics.
-//            // 2. Method names have been improved.
-//            Iterator<JSONObject> iterator = companyList.iterator();
-//            while (iterator.hasNext()) {
-//                System.out.println(iterator.next());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package common;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Loader {
+    public Loader() {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("/Users/Shared/crunchify.json"));
+
+            // A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
+            JSONObject jsonObject = (JSONObject) obj;
+
+            // A JSON array. JSONObject supports java.util.List interface.
+            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+
+            // An iterator over a collection. Iterator takes the place of Enumeration in the Java Collections Framework.
+            // Iterators differ from enumerations in two ways:
+            // 1. Iterators allow the caller to remove elements from the underlying collection during the iteration with well-defined semantics.
+            // 2. Method names have been improved.
+            Iterator<JSONObject> iterator = companyList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
